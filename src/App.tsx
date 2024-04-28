29e8609a854ac704fef682601a2af33c839652dd
TheVal <rajaniemivaltteri@gmail.com>
2024-04-28 13:07:13 +0300

    Refactored data fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,120 @@ import Footer from "./elements/Footer";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Notifications from "./pages/Notifications";
+import { useState, useRef, useEffect } from "react";
+import Timings from "./components/Timings";
+import { PriceData } from "./types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [data, setData] = useState<PriceData | undefined>(undefined); // Price data
+
+  const timeout = useRef<null | ReturnType<typeof setTimeout>>(null); // Timeout for fetching new data
+  const dateChangeHandle = useRef<null | ReturnType<typeof setTimeout>>(null); // Interval for checking if the date has changed
+  const [dataRequiresUpdate, setDataRequiresUpdate] = useState<null | boolean>(
+    null
+  ); // If the data requires an update
+  const [dataLoadingReady, setDataLoadingReady] = useState<boolean>(false); // If the data is ready to be loaded
+
+  useEffect(() => {
+    if (data == null) {
+      // init data
+      getNewData();
+      Timings.day_mumber = new Date().getDate();
+    }
+
+    async function getNewData() {
+      fetch("https://api.epossu.fi/v2/marketData")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data === null) {
+            setDataLoadingReady(true);
+            throw new Error(
+              "Palvelussa tapahtui virhe. Yritä myöhemmin uudelleen."
+            );
+          }
+
+          data = data.data; // setting the data to the data.data
+
+          // if the data for tomorrow is not ok, we set the dataRequiresUpdate to true and schedule a new data fetch'
+          if (data.tomorrow.data_ok === false) {
+            setDataRequiresUpdate(true);
+            scheduleNewDataFetch();
+          } else {
+            setDataRequiresUpdate(false);
+            scheduleNewDataFetch(true);
+          }
+
+          // setting the data and error to null
+          setDataLoadingReady(true);
+          setData(data);
+        })
+        .catch(() => {
+          setDataLoadingReady(true);
+          setData(undefined);
+        });
+    }
+
+    function scheduleNewDataFetch(forceTomorrow: boolean = false) {
+      if (timeout.current !== null) clearTimeout(timeout.current);
+
+      // Set a timeout for fetching new data
+      timeout.current = setTimeout(() => {
+        getNewData();
+      }, Timings.getNextDayPriceTime(forceTomorrow));
+    }
+
+    function handleDateChange() {
+      if (dateChangeHandle.current !== null)
+        clearTimeout(dateChangeHandle.current);
+
+      if (Timings.day_mumber !== new Date().getDate()) {
+        Timings.day_mumber = new Date().getDate();
+        getNewData();
+      }
+
+      dateChangeHandle.current = setTimeout(() => {
+        handleDateChange();
+      }, Timings.getTimeLeftToDateChange());
+    }
+
+    if (dateChangeHandle.current === null && data !== null) {
+      dateChangeHandle.current = setTimeout(() => {
+        handleDateChange();
+      }, 2000);
+    }
+
+    return () => {
+      if (timeout.current !== null) clearTimeout(timeout.current);
+      if (dateChangeHandle.current !== null)
+        clearTimeout(dateChangeHandle.current);
+    };
+  }, [data, dataRequiresUpdate]);
+
   return (
     <Router>
       <div className="wrap">
         <div className="container">
           <div className="row">
             <Routes>
-              <Route path="/" element={<Main />} />
+              {dataLoadingReady ? (
+                <Route path="/" element={<Main data={data} />} />
+              ) : (
+                <Route
+                  path="/"
+                  element={
+                    <div className="wrap">
+                      <div className="container">
+                        <section className="row center loading">
+                          <FontAwesomeIcon icon={faSpinner} spin size="10x" />
+                          <p className="loading-text">Tietoja ladataan...</p>
+                        </section>
+                      </div>
+                    </div>
+                  }
+                />
+              )}
               <Route path="/tietoa" element={<About />} />
               <Route path="/api" element={<ApiDocs />} />
               <Route path="/ilmoitukset" element={<Notifications />} />
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,14 +1,12 @@
 import {
   faCalendarDay,
   faChartArea,
-  faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons/faInfoCircle";
 import { PriceData } from "../types";
 import Chart from "../elements/Chart";
-import Timings from "../components/Timings";
 import { colorMap, colorizePrice } from "../components/Colorizer";
 import "../css/pages/Main.scss";
 import Header from "../elements/Header";
@@ -17,94 +15,20 @@ import NextPrice from "../elements/PriceBoxes/NextPrice";
 import GeneralInfo from "../elements/PriceBoxes/GeneralInfo";
 import TomorrowInfo from "../elements/PriceBoxes/TomorrowsInfo";
 
-const Main = () => {
+interface MainProps {
+  data: PriceData | undefined;
+}
+
+const Main = ({ data }: MainProps) => {
   document.title = "Pörssisähkön ajankohtaiset hinnat - epossu.fi";
 
   const [error, setError] = useState<number | string>(0); // Error message
-  const [data, setData] = useState<PriceData | undefined>(undefined); // Price data
-  const timeout = useRef<null | ReturnType<typeof setTimeout>>(null); // Timeout for fetching new data
-  const dateChangeHandle = useRef<null | ReturnType<typeof setTimeout>>(null); // Interval for checking if the date has changed
-  const [dataRequiresUpdate, setDataRequiresUpdate] = useState<null | boolean>(
-    null
-  ); // If the data requires an update
 
   useEffect(() => {
-    // if we don't have data, fetching it and setting the current days number
-    if (data == null) {
-      getNewData();
-      Timings.day_mumber = new Date().getDate();
+    if (data === undefined || data == null) {
+      setError("Tietoja ei voitu ladata virheen vuoksi.");
     }
-
-    // function for fetching new data
-    async function getNewData() {
-      fetch("https://api.epossu.fi/v2/marketData")
-        .then((response) => response.json())
-        .then((data) => {
-          // if the data is null, we set an error
-
-          if (data === null) {
-            throw new Error(
-              "Palvelussa tapahtui virhe. Yritä myöhemmin uudelleen."
-            );
-          } else {
-            data = data.data; // setting the data to the data.data
-            // if the data for tomorrow is not ok, we set the dataRequiresUpdate to true and schedule a new data fetch
-            if (data.tomorrow.data_ok === false) {
-              setDataRequiresUpdate(true);
-              scheduleNewDataFetch();
-            } else {
-              setDataRequiresUpdate(false);
-              scheduleNewDataFetch(true);
-            }
-
-            // setting the data and error to null
-            setData(data);
-            setError(0);
-          }
-        })
-        .catch((error) => {
-          setError(error.message);
-          setData(undefined);
-        });
-    }
-
-    function scheduleNewDataFetch(forceTomorrow: boolean = false) {
-      if (timeout.current !== null) clearTimeout(timeout.current);
-
-      // Set a timeout for fetching new data
-      timeout.current = setTimeout(() => {
-        getNewData();
-      }, Timings.getNextDayPriceTime(forceTomorrow));
-    }
-
-    function handleDateChange() {
-      if (dateChangeHandle.current !== null)
-        clearTimeout(dateChangeHandle.current);
-
-      if (Timings.day_mumber !== new Date().getDate()) {
-        Timings.day_mumber = new Date().getDate();
-        getNewData();
-      }
-
-      dateChangeHandle.current = setTimeout(() => {
-        handleDateChange();
-      }, Timings.getTimeLeftToDateChange());
-    }
-
-    if (dateChangeHandle.current === null && data !== null) {
-      dateChangeHandle.current = setTimeout(() => {
-        handleDateChange();
-      }, 2000);
-    }
-  }, [data, dataRequiresUpdate]);
-
-  useEffect(() => {
-    return () => {
-      if (timeout.current !== null) clearTimeout(timeout.current);
-      if (dateChangeHandle.current !== null)
-        clearTimeout(dateChangeHandle.current);
-    };
-  }, [timeout, dateChangeHandle]);
+  }, [data]);
 
   return (
     <>
@@ -120,7 +44,7 @@ const Main = () => {
           <p>{error}</p>
         </div>
       )}
-      {data !== undefined ? (
+      {data !== undefined && (
         <>
           <section className="col-row header">
             {data !== undefined ? (
@@ -203,15 +127,6 @@ const Main = () => {
             </div>
           </section>
         </>
-      ) : (
-        <div className="wrap">
-          <div className="container">
-            <section className="row center loading">
-              <FontAwesomeIcon icon={faSpinner} spin size="10x" />
-              <p className="loading-text">Tietoja ladataan...</p>
-            </section>
-          </div>
-        </div>
       )}
     </>
   );
